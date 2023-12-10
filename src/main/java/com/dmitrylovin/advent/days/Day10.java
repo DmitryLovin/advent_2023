b1bf3227bad3985bfc8047295bf315a7ba4c4852
dmitrylovin <dmitrylovin@gmail.com>
2023-12-10 22:20:34 +0600

    Day 10 opti

--- src/main/java/com/dmitrylovin/advent/days/Day10.java
+++ src/main/java/com/dmitrylovin/advent/days/Day10.java
@@ -6,15 +6,15 @@ import java.util.*;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class Day10 extends Day{
-    static final Map<Character, Point[]> DIR_MAP = new HashMap<>(){{
-        put('|', new Point[]{new Point(0,1),new Point(0,-1)});
-        put('-', new Point[]{new Point(1,0),new Point(-1,0)});
-        put('L', new Point[]{new Point(0,-1),new Point(1,0)});
-        put('J', new Point[]{new Point(-1,0),new Point(0,-1)});
-        put('7', new Point[]{new Point(-1,0),new Point(0,1)});
-        put('F', new Point[]{new Point(1,0),new Point(0,1)});
-        put('.', new Point[]{new Point(0,0),new Point(0,0)});
+public class Day10 extends Day {
+    static final Map<Character, Point[]> DIR_MAP = new HashMap<>() {{
+        put('|', new Point[]{new Point(0, 1), new Point(0, -1)});
+        put('-', new Point[]{new Point(1, 0), new Point(-1, 0)});
+        put('L', new Point[]{new Point(0, -1), new Point(1, 0)});
+        put('J', new Point[]{new Point(-1, 0), new Point(0, -1)});
+        put('7', new Point[]{new Point(-1, 0), new Point(0, 1)});
+        put('F', new Point[]{new Point(1, 0), new Point(0, 1)});
+        put('.', new Point[]{new Point(0, 0), new Point(0, 0)});
     }};
 
     public Day10() {
@@ -22,43 +22,43 @@ public class Day10 extends Day{
     }
 
     @Override
-    public void calculate(){
-        calculateWithBenchmark(5000);
+    public void calculate() {
+        calculateWithBenchmark(1000);
     }
 
     @Override
     protected long getResult(int part, String... inputData) {
         Field field = new Field(inputData[0].length(), inputData.length);
 
-        IntStream.range(0, inputData.length).boxed().parallel().forEach((y)->{
-            for(int x = 0; x < inputData[y].length(); x++){
+        IntStream.range(0, inputData.length).boxed().parallel().forEach((y) -> {
+            for (int x = 0; x < inputData[y].length(); x++) {
                 field.addPipe(x, y, inputData[y].charAt(x));
             }
         });
 
-        Map<Point, Integer> connected = new HashMap<>();
+        Set<Point> connected = new HashSet<>();
         List<Point> polyPoints = new ArrayList<>();
-        connected.put(field.start, 0);
+        connected.add(field.start);
         polyPoints.add(field.start);
         Pipe pipe = field.findConnectedToStart();
-        connected.put(pipe.pos, 1);
+        connected.add(pipe.pos);
         polyPoints.add(pipe.pos);
 
-        int i = 2;
-        while(true){
+        while (true) {
             boolean found = false;
-            for(Point point: pipe.connected){
-                if(connected.containsKey(point)){
-                    continue;}
+            for (Point point : pipe.connected) {
+                if (connected.contains(point)) {
+                    continue;
+                }
                 found = true;
-                connected.put(point, i++);
+                connected.add(point);
                 pipe = field.pipes[point.y][point.x];
-                if(part == 1 && !(pipe.key == '|' || pipe.key == '-'))
+                if (part == 1 && !(pipe.key == '|' || pipe.key == '-'))
                     polyPoints.add(point);
             }
-            if(!found) break;
+            if (!found) break;
         }
-        if(part == 0){
+        if (part == 0) {
             return connected.size() / 2;
         }
 
@@ -69,29 +69,54 @@ public class Day10 extends Day{
         );
 
         int[] xPoints = new int[polyPoints.size()];
-        int[] yPoints = new int[polyPoints.size()];
-        int nPoints = polyPoints.size();
+        int[] yPoints = new int[xPoints.length];
+        int nPoints = xPoints.length;
 
-        for(int j = 0; j < polyPoints.size(); j++){
-            xPoints[j] = polyPoints.get(j).x;
-            yPoints[j] = polyPoints.get(j).y;
+        int minX = field.pipes[0].length;
+        int maxX = 0;
+
+        int minY = field.pipes.length;
+        int maxY = 0;
+
+        for (int z = 0; z < nPoints; z++) {
+            xPoints[z] = polyPoints.get(z).x;
+            yPoints[z] = polyPoints.get(z).y;
+
+            if (xPoints[z] < minX)
+                minX = xPoints[z];
+
+            if (xPoints[z] > maxX)
+                maxX = xPoints[z];
+
+            if (yPoints[z] < minY)
+                minY = yPoints[z];
+
+            if (yPoints[z] > maxY)
+                maxY = yPoints[z];
         }
 
         Graphics2D g2d = image.createGraphics();
-        g2d.setColor(Color.GREEN);
+        g2d.setColor(Color.WHITE);
         g2d.fillPolygon(xPoints, yPoints, nPoints);
-        g2d.setColor(Color.BLACK);
-        g2d.drawPolygon(xPoints, yPoints, nPoints);
 
         int count = 0;
 
-        for(int y = 0; y<field.pipes.length; y++){
-            for(int x = 0; x < field.pipes[y].length; x++){
-                int rgb = image.getRGB(x,y);
-                if(rgb==Color.GREEN.getRGB())
+        for (int y = minY + 1; y < maxY; y++) {
+            for (int x = minX + 1; x < maxX; x++) {
+                if (connected.contains(new Point(x, y)))
+                    continue;
+                int rgb = image.getRGB(x, y);
+                if (rgb == -1)
                     count++;
             }
         }
+
+        /*try {
+            ImageIO.write(image,"PNG",new File(String.format("./out_%s.png", System.nanoTime())));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+
         return count;
     }
 
@@ -99,25 +124,25 @@ public class Day10 extends Day{
         Point start;
         Pipe[][] pipes;
 
-        Field(int width, int height){
+        Field(int width, int height) {
             this.pipes = new Pipe[height][width];
         }
 
-        void addPipe(int x, int y, char key){
+        void addPipe(int x, int y, char key) {
             pipes[y][x] = new Pipe(x, y, key);
-            if(key == 'S')
+            if (key == 'S')
                 start = new Point(x, y);
         }
 
-        Pipe findConnectedToStart(){
-            for(int x = start.x - 1; x <= start.x + 1; x++){
-                for(int y = start.y - 1; y <= start.y + 1; y++){
-                    if(x < 0 || y < 0)
+        Pipe findConnectedToStart() {
+            for (int x = start.x - 1; x <= start.x + 1; x++) {
+                for (int y = start.y - 1; y <= start.y + 1; y++) {
+                    if (x < 0 || y < 0)
                         continue;
-                    if(x == start.x && y == start.y)
+                    if (x == start.x && y == start.y)
                         continue;
-                    for(Point point : pipes[y][x].connected){
-                        if(point.equals(start))
+                    for (Point point : pipes[y][x].connected) {
+                        if (point.equals(start))
                             return pipes[y][x];
                     }
                 }
@@ -131,34 +156,34 @@ public class Day10 extends Day{
         Point[] connected = new Point[2];
         char key;
 
-        Pipe(int x, int y, char dir){
+        Pipe(int x, int y, char dir) {
             key = dir;
             pos = new Point(x, y);
             Point[] dirs;
-            if((dirs = DIR_MAP.get(dir)) != null){
-                for(int i=0;i<2;i++){
+            if ((dirs = DIR_MAP.get(dir)) != null) {
+                for (int i = 0; i < 2; i++) {
                     connected[i] = pos.add(dirs[i]);
                 }
             }
         }
     }
 
-    record Point(int x, int y){
+    record Point(int x, int y) {
         @Override
-        public String toString(){
+        public String toString() {
             return String.format("(%d : %d)", x, y);
         }
 
         @Override
-        public int hashCode(){
+        public int hashCode() {
             return this.x * 1000 + y;
         }
 
-        public boolean equals(Point point){
+        public boolean equals(Point point) {
             return x == point.x && y == point.y();
         }
 
-        public Point add(Point point){
+        public Point add(Point point) {
             return new Point(x + point.x, y + point.y);
         }
     }
